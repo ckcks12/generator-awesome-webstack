f3854d73f9d9249743c2d26ddf849ff3a32c701c
ckcks12 <wyoducc@gmail.com>
2018-09-17 15:14:49 +0900

    add Sequelize & pm2 watch debugging

--- generators/app/templates/express/index.js
+++ generators/app/templates/express/index.js
@@ -3,7 +3,30 @@ const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const http = require('http')
 const routes = require('./routes')
+const Sequelize = require('sequelize')
 
+
+const sequelize = new Sequelize(
+    'test',
+    'root',
+    'password',
+    {
+        'host': 'localhost',
+        'dialect': 'mysql'
+    }
+)
+const List = sequelize.define('list', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    value: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+})
+List.sync()
 
 let app = express()
 let server = http.createServer(app)
@@ -16,6 +39,12 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    req.sequelize = sequelize
+    req.list = List
+    next()
+})
 
 app.use('/', routes)
 
--- generators/app/templates/express/routes/index.js
+++ generators/app/templates/express/routes/index.js
@@ -5,4 +5,17 @@ router.get('/', (req, res, next) => {
     res.send('hello world')
 })
 
+router.get('/add/:value', (req, res, next) => {
+    req.list.create({
+        value: req.params.value
+    })
+    res.send('added')
+})
+
+router.get('/list', (req, res, next) => {
+    req.list.findAll().then((list) => {
+        res.json(list)
+    })
+})
+
 module.exports = router
