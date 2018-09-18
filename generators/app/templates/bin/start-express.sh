60408abbbaa1db472a30d374bf1b1214372b7acb
ckcks12 <wyoducc@gmail.com>
2018-09-18 22:19:42 +0900

    add yarn for installing node modules before run

--- generators/app/templates/bin/start-express.sh
+++ generators/app/templates/bin/start-express.sh
@@ -11,11 +11,11 @@ img="node:alpine"
 cmd="echo command goes here"
 
 if [ "$1" = dev ]; then
-    cmd="npm --prefix /code run dev"
+    cmd="yarn && npm --prefix /code run dev"
 elif [ "$1" = test ]; then
-    cmd="npm --prefix /code run test"
+    cmd="yarn && npm --prefix /code run test"
 else
-    cmd="npm --prefix /code run start"
+    cmd="yarn && npm --prefix /code run start"
 fi
 
 docker run $i $t $name $rm $net $e $v $img $cmd
