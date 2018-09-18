d56494f55fa95975960cc86f7b47cfd7a7121dd2
ckcks12 <wyoducc@gmail.com>
2018-09-18 22:45:33 +0900

    fix npm install error and directory error

--- generators/app/templates/bin/start-express.sh
+++ generators/app/templates/bin/start-express.sh
@@ -7,15 +7,16 @@ rm="--rm"
 net="--net=host"
 v="-v `pwd`/express:/code"
 e=""
+w="-w /code/express"
 img="node:alpine"
 cmd="echo command goes here"
 
 if [ "$1" = dev ]; then
-    cmd="yarn && npm --prefix /code run dev"
+    cmd="yarn && yarn dev"
 elif [ "$1" = test ]; then
-    cmd="yarn && npm --prefix /code run test"
+    cmd="yarn && yarn test"
 else
-    cmd="yarn && npm --prefix /code run start"
+    cmd="yarn && yarn start"
 fi
 
-docker run $i $t $name $rm $net $e $v $img $cmd
+docker run $i $t $name $net $v $e $w $img sh -c "$cmd"
